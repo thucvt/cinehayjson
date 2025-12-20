fa300930136ff8e5d8355a4a323ada052ca37170
thucvt <mrkempq@gmail.com>
2025-12-20 23:36:10 +0700

    Create series.js

---
+++ series.js
@@ -0,0 +1,155 @@
+// series.js (generated locally)
+const series = {
+  "id": 153869,
+  "name": "Tử Xuyên",
+  "original_name": "紫川",
+  "overview": "Truyền kì máu lửa qua từng thời đại: Trong đế quốc của loài người, gia tộc thần bí ở phương Đông xa xôi, Tử Xuyên Tam Kiệt phất lên! Anh cả Đế Lâm chín chắn đa mưu, tàn khốc lí trí, anh hai Sterling chỉ huy một đội quân, có lòng trung thành với nước, em út Tử Xuyên Tú bị người đời gọi với cái tên “Vô Lại”, trí dũng song toàn. Gia tộc Tử Xuyên loạn trong giặc ngoài, Tử Xuyên Tam Kiệt mỗi người đều có thế mạnh riêng: Trong thời khắc nước sôi lửa bỏng, đánh bại Ma tộc hung ác, xông vào hang hổ truy sát tên phản đồ Lôi Hồng đến cùng; Tư Đặc Lâm lấy gia tộc làm gốc, vừa vô tình vừa trọng tình lưu luyến người yêu... Loài người, Ma tộc, Thú tộc. Mỗi tộc ở mỗi phương khác nhau, tranh chấp lẫn nhau trên mảnh đất Đại Lục này, xích mích không ngừng. Khi máu và lửa hòa vào nhau, đao và kiếm va chạm, tạo ra câu chuyện sử thi bi tráng-Tử Xuyên.",
+  "poster_path": "https://image.tmdb.org/t/p/w500/ouv4VvMNFNoSsyE0SmDeabOjM4O.jpg",
+  "backdrop_path": "https://image.tmdb.org/t/p/original/fhTDNfkicAu8lMLjYqtZYJnhcXa.jpg",
+  "first_air_date": "2021-12-21",
+  "vote_average": 8.5,
+  "genres": [
+    "Phim Hoạt Hình",
+    "Sci-Fi & Fantasy"
+  ],
+  "production_countries": [
+    "China"
+  ],
+  "production_companies": [
+    "Build Dream",
+    "Tencent Penguin Pictures"
+  ],
+  "spoken_languages": [
+    "Mandarin"
+  ],
+  "original_language": "zh",
+  "status": "Returning Series",
+  "tagline": "",
+  "homepage": "https://v.qq.com/detail/t/mzc00200iawcmn2.html",
+  "certification": "",
+  "director": "",
+  "cast_top10": [
+    "Tang Bao",
+    "马正阳",
+    "姜广涛",
+    "宝木中阳",
+    "阿杰",
+    "喬詩語",
+    "Liangwei Hu",
+    "高启帆",
+    "Yuanfei Mei",
+    "Dong Zhou"
+  ],
+  "trailer_youtube_key": "",
+  "trailer_youtube_url": "",
+  "episodes": [
+    {
+      "season": 2,
+      "episode": 1,
+      "filename": "Zi.Chuan.S02E01.2025.4K.H265.Vietsub.TM_chiphu.mkv",
+      "dropbox_link": "https://www.dropbox.com/scl/fi/u4og7pq9wohw1cixab97t/Zi.Chuan.S02E01.2025.4K.H265.Vietsub.TM_chiphu.mkv?rlkey=1q6lucgmwigemvfakwakodlmh&dl=1"
+    },
+    {
+      "season": 2,
+      "episode": 2,
+      "filename": "Zi.Chuan.S02E02.2025.4K.H265.Vietsub.TM_chiphu.mkv",
+      "dropbox_link": "https://www.dropbox.com/scl/fi/0o06ouvwulep8unf449zr/Zi.Chuan.S02E02.2025.4K.H265.Vietsub.TM_chiphu.mkv?rlkey=3wlzwtda9w8wnmj7vdj1o2blp&dl=1"
+    },
+    {
+      "season": 2,
+      "episode": 3,
+      "filename": "Zi.Chuan.S02E03.2025.4K.H265.Vietsub.TM_chiphu.mkv",
+      "dropbox_link": "https://www.dropbox.com/scl/fi/07ot0ojnft51od84mt5e0/Zi.Chuan.S02E03.2025.4K.H265.Vietsub.TM_chiphu.mkv?rlkey=q4pgwo9jvo7j39si8vhngmefu&dl=1"
+    },
+    {
+      "season": 2,
+      "episode": 4,
+      "filename": "Zi.Chuan.S02E04.2025.4K.H265.Vietsub.TM_chiphu.mkv",
+      "dropbox_link": "https://www.dropbox.com/scl/fi/3bu7cnejjac6b7a8o21zl/Zi.Chuan.S02E04.2025.4K.H265.Vietsub.TM_chiphu.mkv?rlkey=3ddml5yajpl4eb7x4da6s9zze&dl=1"
+    },
+    {
+      "season": 2,
+      "episode": 5,
+      "filename": "Zi.Chuan.S02E05.2025.4K.H265.Vietsub.TM_chiphu.mkv",
+      "dropbox_link": "https://www.dropbox.com/scl/fi/uklj8ufc6r64wvsyjhrtu/Zi.Chuan.S02E05.2025.4K.H265.Vietsub.TM_chiphu.mkv?rlkey=vpbs9cwajufozcpoj6u4uuqjc&dl=1"
+    },
+    {
+      "season": 2,
+      "episode": 6,
+      "filename": "Zi.Chuan.S02E06.2025.4K.H265.Vietsub.TM_chiphu.mkv",
+      "dropbox_link": "https://www.dropbox.com/scl/fi/ybtb0xbeiz9ulwh8uqn8j/Zi.Chuan.S02E06.2025.4K.H265.Vietsub.TM_chiphu.mkv?rlkey=ah0n2g3hf9wkdxkkzzzvkd7qk&dl=1"
+    },
+    {
+      "season": 2,
+      "episode": 7,
+      "filename": "Zi.Chuan.S02E07.2025.4K.H265.Vietsub.TM_chiphu.mkv",
+      "dropbox_link": "https://www.dropbox.com/scl/fi/ii1z43opy5fk465e3pue4/Zi.Chuan.S02E07.2025.4K.H265.Vietsub.TM_chiphu.mkv?rlkey=5fs1xiszkwjfs4w9m34p4k7qc&dl=1"
+    },
+    {
+      "season": 2,
+      "episode": 8,
+      "filename": "Zi.Chuan.S02E08.2025.4K.H265.Vietsub.TM_chiphu.mkv",
+      "dropbox_link": "https://www.dropbox.com/scl/fi/yo39pdbcidi5ciq8icg2c/Zi.Chuan.S02E08.2025.4K.H265.Vietsub.TM_chiphu.mkv?rlkey=ign95fwmsb89954xlwhjgf5fy&dl=1"
+    },
+    {
+      "season": 2,
+      "episode": 9,
+      "filename": "Zi.Chuan.S02E09.2025.4K.H265.Vietsub.TM_chiphu.mkv",
+      "dropbox_link": "https://www.dropbox.com/scl/fi/qvptj9dacxqckxsrnnfdq/Zi.Chuan.S02E09.2025.4K.H265.Vietsub.TM_chiphu.mkv?rlkey=9yijfbpyaxpl0bylkw9p8t8qm&dl=1"
+    },
+    {
+      "season": 2,
+      "episode": 10,
+      "filename": "Zi.Chuan.S02E10.2025.4K.H265.Vietsub.TM_chiphu.mkv",
+      "dropbox_link": "https://www.dropbox.com/scl/fi/v2op7p762gmht57rowf8c/Zi.Chuan.S02E10.2025.4K.H265.Vietsub.TM_chiphu.mkv?rlkey=mbm0tw5toufbiwf31g1o1zr5u&dl=1"
+    },
+    {
+      "season": 2,
+      "episode": 11,
+      "filename": "Zi.Chuan.S02E11.2025.4K.H265.Vietsub.TM_chiphu.mkv",
+      "dropbox_link": "https://www.dropbox.com/scl/fi/hi8ujfgbe65zgn5ou81h5/Zi.Chuan.S02E11.2025.4K.H265.Vietsub.TM_chiphu.mkv?rlkey=tyt6frdij7b1pxdh3wf8yo5bl&dl=1"
+    },
+    {
+      "season": 2,
+      "episode": 12,
+      "filename": "Zi.Chuan.S02E12.2025.4K.H265.Vietsub.TM_chiphu.mkv",
+      "dropbox_link": "https://www.dropbox.com/scl/fi/2uidwlrtgez0rj18ne0jq/Zi.Chuan.S02E12.2025.4K.H265.Vietsub.TM_chiphu.mkv?rlkey=is8cnnqh5lzoo9b2gwenqf47x&dl=1"
+    },
+    {
+      "season": 2,
+      "episode": 13,
+      "filename": "Zi.Chuan.S02E13.2025.4K.H265.Vietsub.TM_chiphu.mkv",
+      "dropbox_link": "https://www.dropbox.com/scl/fi/i87ax955b365euj27hw9s/Zi.Chuan.S02E13.2025.4K.H265.Vietsub.TM_chiphu.mkv?rlkey=sw0r2gd10dplkznepnruk0ec5&dl=1"
+    },
+    {
+      "season": 2,
+      "episode": 14,
+      "filename": "Zi.Chuan.S02E14.2025.4K.H265.Vietsub.TM_chiphu.mkv",
+      "dropbox_link": "https://www.dropbox.com/scl/fi/rr92shxisg7qds4ofjson/Zi.Chuan.S02E14.2025.4K.H265.Vietsub.TM_chiphu.mkv?rlkey=l3up0yuwvixqzbsut5vuqdnwk&dl=1"
+    },
+    {
+      "season": 2,
+      "episode": 15,
+      "filename": "Zi.Chuan.S02E15.2025.4K.H265.Vietsub.TM_chiphu.mkv",
+      "dropbox_link": "https://www.dropbox.com/scl/fi/tekv32o2ap5q2l7e1bgei/Zi.Chuan.S02E15.2025.4K.H265.Vietsub.TM_chiphu.mkv?rlkey=jkwfe3a7wu3kpjzncf4jbyese&dl=1"
+    },
+    {
+      "season": 2,
+      "episode": 16,
+      "filename": "Zi.Chuan.S02E16.2025.4K.H265.Vietsub.TM_chiphu.mkv",
+      "dropbox_link": "https://www.dropbox.com/scl/fi/njc6gljtbs8ntnjh28c3e/Zi.Chuan.S02E16.2025.4K.H265.Vietsub.TM_chiphu.mkv?rlkey=u3ic4bxyh54b72j4uh0swz3sw&dl=1"
+    },
+    {
+      "season": 2,
+      "episode": 17,
+      "filename": "Zi.Chuan.S02E17.2025.4K.H265.Vietsub.TM_chiphu.mkv",
+      "dropbox_link": "https://www.dropbox.com/scl/fi/23nko2v4qjdu1rbje6uqr/Zi.Chuan.S02E17.2025.4K.H265.Vietsub.TM_chiphu.mkv?rlkey=litcqxhyembkr0unzoolm32uk&dl=1"
+    },
+    {
+      "season": 2,
+      "episode": 18,
+      "filename": "Zi.Chuan.S02E18.2025.4K.H265.Vietsub.TM_chiphu.mkv",
+      "dropbox_link": "https://www.dropbox.com/scl/fi/8lt5s27nct4rtpkpq74r1/Zi.Chuan.S02E18.2025.4K.H265.Vietsub.TM_chiphu.mkv?rlkey=vl0aw8l9ssv0sao7ekxzkxvq8&dl=1"
+    }
+  ]
+};
